10f3976e870e07c192de54f6a7a34c1460aa440a
future-tech-holic <165118807+future-tech-holic@users.noreply.github.com>
2024-03-27 13:06:39 +0900

    Gpt (#1)
    * commit
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Chris Song <sjhshy@gmail.com>

---
+++ submissions/.dfx/ic/canisters/gpt_wrapper/service.did.js
@@ -0,0 +1,26 @@
+export const idlFactory = ({ IDL }) => {
+  const HttpHeader = IDL.Record({ 'value' : IDL.Text, 'name' : IDL.Text });
+  const HttpResponsePayload = IDL.Record({
+    'status' : IDL.Nat,
+    'body' : IDL.Vec(IDL.Nat8),
+    'headers' : IDL.Vec(HttpHeader),
+  });
+  const TransformArgs = IDL.Record({
+    'context' : IDL.Vec(IDL.Nat8),
+    'response' : HttpResponsePayload,
+  });
+  const CanisterHttpResponsePayload = IDL.Record({
+    'status' : IDL.Nat,
+    'body' : IDL.Vec(IDL.Nat8),
+    'headers' : IDL.Vec(HttpHeader),
+  });
+  return IDL.Service({
+    'greet' : IDL.Func([IDL.Text], [IDL.Text], []),
+    'transform' : IDL.Func(
+        [TransformArgs],
+        [CanisterHttpResponsePayload],
+        ['query'],
+      ),
+  });
+};
+export const init = ({ IDL }) => { return []; };
