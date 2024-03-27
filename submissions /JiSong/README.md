10dabb7e7d2e11cf55984d7b8c28a1b9a94de3b7
JisongImSorry <116711095+JisongImSorry@users.noreply.github.com>
2024-03-27 14:55:53 +0900

    JiSong commit

---
+++ submissions /JiSong/README.md
@@ -0,0 +1,55 @@
+# Crosschain Scanner
+
+GitHub Codespaces
+
+[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/JisongImSorry/evm-rpc-rust/?quickstart=1)
+
+# Project description
+A cross-chain block scanner, uniquely built on ICP mainnet
+
+1. Fully on ICP Mainnet
+Frontend is completely executable on ICP Mainnet!
+
+2. Support for multi-chain
+Currently it only supports ETH, but is planning to support for BNB, ARB ... Multi-chain support awaiting!
+
+3. EVM-Compatible
+EVM-Compatible Mainnets can easily be uploaded
+
+# Track
+Track 1. xChain dapps
+
+# Team participants
+Name: Jiwoong Song
+
+Github username: JisongImSorry
+
+  [https://github.com/JisongImSorry]
+
+Discord IDs: 
+  jisongimsorry
+
+
+
+# Canister ID(s) of mainnet deployment
+  Frontend canister
+    
+    frontend: https://zgca2-pyaaa-aaaal-qdwva-cai.icp0.io/
+
+    
+  Backend canister 
+    
+    backend: https://a4gq6-oaaaa-aaaab-qaa4q-cai.raw.icp0.io/?id=zpblg-zqaaa-aaaal-qdwuq-cai
+    
+    evm_rpc: https://a4gq6-oaaaa-aaaab-qaa4q-cai.raw.icp0.io/?id=7hfb6-caaaa-aaaar-qadga-cai
+
+# Feedback part
+what have you learned
+  1. Learned how to use rust language to effectively handle ICP
+  2. Learned how to effectively utilize ICP to achieve web goals
+what were the challenges you ran into
+  1. Rust language was pretty hard for me to learn
+  2. The development environment had to be set using the github codespace, making it hard for me to implement
+what are you proud of
+  1. Deployed my full frontend to ICP Mainnet
+  2. Deployed and implemented RUST Backend into ICP Mainnet
---
+++ submissions /JiSong/src/App.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import './App.scss';
+import rustLogo from './assets/rust.svg';
+import reactLogo from './assets/react.svg';
+import ethLogo from './assets/eth.svg';
+import { backend } from './declarations/backend';
+import { Block } from './declarations/backend/backend.did';
+
+// JSON viewer component
+import { JsonView, allExpanded, defaultStyles } from 'react-json-view-lite';
+import 'react-json-view-lite/dist/index.css';
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [block, setBlock] = useState<Block | undefined>();
+  const [error, setError] = useState<string | undefined>();
+
+  const fetchBlock = async () => {
+    try {
+      setLoading(true);
+      setError(undefined);
+      const block = await backend.getLatestEthereumBlock();
+      setBlock(block);
+    } catch (err) {
+      console.error(err);
+      setError(String(err));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a
+          href="https://github.com/internet-computer-protocol/evm-rpc-canister#readme"
+          target="_blank"
+        >
+          <img src={ethLogo} className="logo ethereum" alt="Ethereum logo" />
+        </a>
+        <a
+          href="https://internetcomputer.org/docs/current/developer-docs/backend/rust/"
+          target="_blank"
+        >
+          <span className="logo-stack">
+            <img src={rustLogo} className="logo rust" alt="Rust logo" />
+          </span>
+        </a>
+      </div>
+      <h1 style={{ paddingLeft: 36 }}>React + EVM RPC + Rust</h1>
+      <div className="card" style={{ opacity: loading ? 0.5 : 1 }}>
+        <button onClick={fetchBlock}>Get latest block</button>
+        {!!block && (
+          <pre className="json-view">
+            <JsonView
+              data={block}
+              shouldExpandNode={allExpanded}
+              style={{ ...defaultStyles, container: '' }}
+            />
+          </pre>
+        )}
+        {!!error && (
+          <pre style={{ textAlign: 'left', color: 'grey' }}>{error}</pre>
+        )}
+        {!!loading && !block && !error && <div className="loader" />}
+      </div>
+      <p className="read-the-docs">
+        Click on the React, Ethereum, and Rust logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
