da8b53c657810eda6307e5fc4dc5debd765dc59b
JisongImSorry <116711095+JisongImSorry@users.noreply.github.com>
2024-03-27 18:33:03 +0900

    missing code updates

--- submissions /JiSong/src/App.tsx
+++ submissions /JiSong/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.scss';
 import rustLogo from './assets/rust.svg';
 import reactLogo from './assets/react.svg';
@@ -13,13 +13,40 @@ import 'react-json-view-lite/dist/index.css';
 function App() {
   const [loading, setLoading] = useState(false);
   const [block, setBlock] = useState<Block | undefined>();
+  console.log(block)
   const [error, setError] = useState<string | undefined>();
+  const [blocks, setBlocks] = useState<Block[]>([]);
+
+  const [sel, setSel] = useState(-1)
+
+  useEffect(()=>{
+    setInterval(()=>{
+      fetchBlock();
+    }, 5000)
+  }, [])
+
+  useEffect(()=>{
+    if(block){
+      let has = false
+      blocks.forEach((data : any)=>{
+        if(data.receiptsRoot === block.receiptsRoot){
+          has = true
+        }
+      })
+      if(!has){
+        let b = blocks.slice()
+        b.push(block)
+        setBlocks(b)
+      }
+    }
+  }, [block])
 
   const fetchBlock = async () => {
     try {
       setLoading(true);
       setError(undefined);
       const block = await backend.getLatestEthereumBlock();
+      //const block = await backend.getEthereumBlockByNumber();
       setBlock(block);
     } catch (err) {
       console.error(err);
@@ -31,45 +58,28 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a
-          href="https://github.com/internet-computer-protocol/evm-rpc-canister#readme"
-          target="_blank"
-        >
-          <img src={ethLogo} className="logo ethereum" alt="Ethereum logo" />
-        </a>
-        <a
-          href="https://internetcomputer.org/docs/current/developer-docs/backend/rust/"
-          target="_blank"
-        >
-          <span className="logo-stack">
-            <img src={rustLogo} className="logo rust" alt="Rust logo" />
-          </span>
-        </a>
+      <h1 style={{ paddingLeft: 36 }}>Crosschain Scan</h1>
+      <h3 style={{ paddingLeft: 36 }}>currentMode : ETH</h3>
+      <div className="card" style={{boxShadow: "5px 5px 10px", height: "40rem", overflow: 'auto'}}>
+        <div style={{fontWeight: 'bold', marginBottom: 20, display: 'grid', gridTemplateColumns: "repeat(3, minmax(0, 1fr))"}}>
+          <div>Block #</div>
+          <div># of Transactions</div>
+          <div>Miner</div>
+        </div>
+        {blocks.map((item: Block, index: number)=>{return <>
+        <div style={{display: 'grid', gridTemplateColumns: "repeat(3, minmax(0, 1fr))", marginBottom: 10}}>
+          <div>{Number(item.number)}</div>
+          <div onClick={()=>{if(sel === index) setSel(-1) 
+            else setSel(index)}} style={{textDecoration: 'underline', color: 'blue', cursor: 'pointer'}}>{Number(item.transactions.length)}</div>
+          <div>{item.miner}</div>
+        </div>
+        {sel === index && <div>
+            {item.transactions.map((item: any)=>{
+              return <div>{item}</div>
+            })}
+        </div>}
+        </>})}
       </div>
-      <h1 style={{ paddingLeft: 36 }}>React + EVM RPC + Rust</h1>
-      <div className="card" style={{ opacity: loading ? 0.5 : 1 }}>
-        <button onClick={fetchBlock}>Get latest block</button>
-        {!!block && (
-          <pre className="json-view">
-            <JsonView
-              data={block}
-              shouldExpandNode={allExpanded}
-              style={{ ...defaultStyles, container: '' }}
-            />
-          </pre>
-        )}
-        {!!error && (
-          <pre style={{ textAlign: 'left', color: 'grey' }}>{error}</pre>
-        )}
-        {!!loading && !block && !error && <div className="loader" />}
-      </div>
-      <p className="read-the-docs">
-        Click on the React, Ethereum, and Rust logos to learn more
-      </p>
     </div>
   );
 }
